581d1e042014528feebeab8308183f9f0045a76c
king <983563622@qq.com>
2015-07-28 17:48:49 +0800

    Initial Commit

---
+++ BKNavigationBarCleanColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BKNavigationBarCleanColor
+//
+//  Created by apple on 15/7/28.
+//  Copyright (c) 2015年 CloudRoom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
